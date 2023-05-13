2785972844f0b01159ca1d55309fd305ae85c503
Mimi <91705093+shypotatosage@users.noreply.github.com>
2023-05-13 11:19:21 +0700

    Initial Commit

---
+++ Bebek/BebekApp.swift
@@ -0,0 +1,17 @@
+//
+//  BebekApp.swift
+//  Bebek
+//
+//  Created by MacBook Pro on 13/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BebekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bebek/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bebek
+//
+//  Created by MacBook Pro on 13/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
